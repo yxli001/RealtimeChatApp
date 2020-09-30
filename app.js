1f336e05bfa2f9bba79543d7ae35137f6e4225bf
Yixuan Li <lxd520ANNA@gmail.com>
2020-09-30 19:55:46 -0400

    add users display

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
-const app = express();
 const bodyParser = require("body-parser");
+const moment = require("moment");
+const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
@@ -8,37 +9,71 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Variables
+let users = [];
+
+// Root route
 app.get("/", (req, res) => {
 	res.render("index");
 });
 
-let users = [];
-
+// Chat form post route
 app.post("/", (req, res) => {
 	let username = req.body.username;
 	res.redirect("/chat?username=" + username);
 });
 
+// Chat route
 app.get("/chat", (req, res) => {
-	users.push(req.query.username);
-
-	res.render("chat", { user: users, user: req.query.username });
+	res.render("chat", { user: req.query.username });
 });
 
+// When a client connects
 io.on("connection", (socket) => {
-	io.emit("chat message", {
-		msg: users[users.length - 1] + " has connected",
+	// Emits welcome message to the new connected user
+	socket.emit("chat message", {
+		msg: "Welcome to World Wide Chat!",
 		user: "Bot",
+		time: moment().format("h:mm a"),
 	});
-	socket.on("chat message", (msg) => {
-		io.emit("chat message", msg);
-	});
-	socket.on("disconnect", () => {
-		io.emit("chat message", {
-			msg: users[users.length - 1] + " has disconnected",
+
+	// When frontend emits the username of the new client
+	socket.on("username", (username) => {
+		// Push new user to array
+		users.push({ username: username, socketId: socket.id });
+
+		// Tell the frontend to update users list
+		io.emit("update users", users);
+
+		// Emits message to all other users
+		socket.broadcast.emit("chat message", {
+			msg: users[users.length - 1].username + " has connected",
 			user: "Bot",
+			time: moment().format("h:mm a"),
 		});
-		users.pop();
+	});
+
+	// When receive a message from a client
+	socket.on("message", ({ msg, user }) => {
+		// Add the time to the message and emit it to all clients
+		io.emit("chat message", { msg, user, time: moment().format("h:mm a") });
+	});
+
+	// When the connected client disconnects
+	socket.on("disconnect", () => {
+		// Emits a message to all clients
+		io.emit("chat message", {
+			msg:
+				users.find((user) => {
+					return user.socketId == socket.id;
+				}).username + " has disconnected",
+			user: "Bot",
+			time: moment().format("h:mm a"),
+		});
+		users = users.filter((user) => {
+			return user.socketId != socket.id;
+		});
+		io.emit("update users", users);
 	});
 });
 
